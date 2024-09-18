e01e380a1ed6881d72b6b4c5ecc7a359275349c3
KateZzh <ekaterin.pro2@gmail.com>
2024-09-18 10:59:31 +0300

    add HW1

---
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import s from './Message.module.css';
+import { MessageType } from '../HW1';
+
+export type MessageProps = {
+    message: MessageType;
+};
+
+// нужно отобразить приходящие данные
+const Message = (props: MessageProps) => {
+    return (
+        <div id={'hw1-message-' + props.message.id} className={s.message}>
+            <div className={s.imageAndText}>
+                <img id={'hw1-avatar-' + props.message.id} src={props.message.user.avatar} alt='avatar of user' />
+                <div className={s.text}>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>
+                        {props.message.user.name}
+                    </div>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                        {props.message.message.text}
+                    </pre>
+                </div>
+            </div>
+            <div id={'hw1-time-' + props.message.id} className={s.time}>
+                {props.message.message.time}
+            </div>
+        </div>
+    );
+};
+
+export default Message;
