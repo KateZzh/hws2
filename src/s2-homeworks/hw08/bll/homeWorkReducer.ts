dfcb51bc9158e4550e24eff6c685de0cd0761644
KateZzh <ekaterin.pro2@gmail.com>
2024-11-11 12:15:46 +0300

    add HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,24 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            const newState = [...state];
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                newState.sort((a, b) => a.name.localeCompare(b.name));
+            } else {
+                newState.sort((a, b) => b.name.localeCompare(a.name));
+            }
+
+            return newState;
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((el) => el.age > 17);
         }
         default:
-            return state
+            return state;
     }
-}
+};
