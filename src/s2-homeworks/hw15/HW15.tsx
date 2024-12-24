485f1bd4e28211198234b273f7d62da3e5e34a6b
KateZzh <ekaterin.pro2@gmail.com>
2024-12-25 00:06:22 +0300

    add HW15

--- src/s2-homeworks/hw15/HW15.tsx
+++ src/s2-homeworks/hw15/HW15.tsx
@@ -1,96 +1,77 @@
-import React, {useEffect, useState} from 'react'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW15.module.css'
-import axios from 'axios'
-import SuperPagination from './common/c9-SuperPagination/SuperPagination'
-import {useSearchParams} from 'react-router-dom'
-import SuperSort from './common/c10-SuperSort/SuperSort'
-
-/*
-* 1 - дописать SuperPagination
-* 2 - дописать SuperSort
-* 3 - проверить pureChange тестами
-* 3 - дописать sendQuery, onChangePagination, onChangeSort в HW15
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW15 в HW5/pages/JuniorPlus
-* */
+import React, { useEffect, useState } from 'react';
+import s2 from '../../s1-main/App.module.css';
+import s from './HW15.module.css';
+import axios from 'axios';
+import SuperPagination from './common/c9-SuperPagination/SuperPagination';
+import { useSearchParams } from 'react-router-dom';
+import SuperSort from './common/c10-SuperSort/SuperSort';
 
 type TechType = {
-    id: number
-    tech: string
-    developer: string
-}
+    id: number;
+    tech: string;
+    developer: string;
+};
 
 type ParamsType = {
-    sort: string
-    page: number
-    count: number
-}
+    sort: string;
+    page: number;
+    count: number;
+};
 
 const getTechs = (params: ParamsType) => {
     return axios
-        .get<{ techs: TechType[], totalCount: number }>(
-            'https://samurai.it-incubator.io/api/3.0/homework/test3',
-            {params}
-        )
-        .catch((e) => {
-            alert(e.response?.data?.errorText || e.message)
+        .get<{ techs: TechType[]; totalCount: number }>('https://samurai.it-incubator.io/api/3.0/homework/test3', {
+            params,
         })
-}
+        .catch((e) => {
+            alert(e.response?.data?.errorText || e.message);
+        });
+};
 
 const HW15 = () => {
-    const [sort, setSort] = useState('')
-    const [page, setPage] = useState(1)
-    const [count, setCount] = useState(4)
-    const [idLoading, setLoading] = useState(false)
-    const [totalCount, setTotalCount] = useState(100)
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [techs, setTechs] = useState<TechType[]>([])
+    const [sort, setSort] = useState('');
+    const [page, setPage] = useState(1);
+    const [count, setCount] = useState(4);
+    const [idLoading, setLoading] = useState(false);
+    const [totalCount, setTotalCount] = useState(100);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [techs, setTechs] = useState<TechType[]>([]);
 
     const sendQuery = (params: any) => {
-        setLoading(true)
-        getTechs(params)
-            .then((res) => {
-                // делает студент
-
-                // сохранить пришедшие данные
-
-                //
-            })
-    }
+        setLoading(true);
+        getTechs(params).then((res) => {
+            if (res) {
+                if (res.data) {
+                    setTechs(res.data.techs);
+                    setTotalCount(res.data.totalCount);
+                }
+            }
+            setLoading(false);
+        });
+    };
 
     const onChangePagination = (newPage: number, newCount: number) => {
-        // делает студент
-
-        // setPage(
-        // setCount(
-
-        // sendQuery(
-        // setSearchParams(
-
-        //
-    }
+        setPage(newPage);
+        setCount(newCount);
+        sendQuery({ page: newPage, count: newCount });
+        setSearchParams({ page: String(newPage), count: String(newCount) });
+    };
 
     const onChangeSort = (newSort: string) => {
-        // делает студент
-
-        // setSort(
-        // setPage(1) // при сортировке сбрасывать на 1 страницу
-
-        // sendQuery(
-        // setSearchParams(
-
-        //
-    }
+        setSort(newSort);
+        setPage(1);
+        sendQuery({ page: 1, count, sort: newSort });
+        setSearchParams({ page: '1', count: String(count) });
+    };
 
     useEffect(() => {
-        const params = Object.fromEntries(searchParams)
-        sendQuery({page: params.page, count: params.count})
-        setPage(+params.page || 1)
-        setCount(+params.count || 4)
-    }, [])
+        const params = Object.fromEntries(searchParams);
+        sendQuery({ page: params.page, count: params.count });
+        setPage(+params.page || 1);
+        setCount(+params.count || 4);
+    }, []);
 
-    const mappedTechs = techs.map(t => (
+    const mappedTechs = techs.map((t) => (
         <div key={t.id} className={s.row}>
             <div id={'hw15-tech-' + t.id} className={s.tech}>
                 {t.tech}
@@ -100,38 +81,42 @@ const HW15 = () => {
                 {t.developer}
             </div>
         </div>
-    ))
+    ));
 
     return (
         <div id={'hw15'}>
             <div className={s2.hwTitle}>Homework #15</div>
 
             <div className={s2.hw}>
-                {idLoading && <div id={'hw15-loading'} className={s.loading}>Loading...</div>}
+                <div className={s.container}>
+                    {idLoading && <div id={'hw15-loading'} className={s.loading}></div>}
 
-                <SuperPagination
-                    page={page}
-                    itemsCountForPage={count}
-                    totalCount={totalCount}
-                    onChange={onChangePagination}
-                />
+                    {idLoading && <div className={s.background}></div>}
 
-                <div className={s.rowHeader}>
-                    <div className={s.techHeader}>
-                        tech
-                        <SuperSort sort={sort} value={'tech'} onChange={onChangeSort}/>
+                    <SuperPagination
+                        page={page}
+                        itemsCountForPage={count}
+                        totalCount={totalCount}
+                        onChange={onChangePagination}
+                    />
+
+                    <div className={s.rowHeader}>
+                        <div className={s.techHeader}>
+                            tech
+                            <SuperSort sort={sort} value={'tech'} onChange={onChangeSort} />
+                        </div>
+
+                        <div className={s.developerHeader}>
+                            developer
+                            <SuperSort sort={sort} value={'developer'} onChange={onChangeSort} />
+                        </div>
                     </div>
 
-                    <div className={s.developerHeader}>
-                        developer
-                        <SuperSort sort={sort} value={'developer'} onChange={onChangeSort}/>
-                    </div>
+                    {mappedTechs}
                 </div>
-
-                {mappedTechs}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW15
+export default HW15;
--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,65 +1,71 @@
-import React from 'react'
-import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
-import {Pagination} from '@mui/material'
-import s from './SuperPagination.module.css'
+import React from 'react';
+import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect';
+import { Pagination } from '@mui/material';
+import s from './SuperPagination.module.css';
 
 export type SuperPaginationPropsType = {
-    id?: string
-    page: number
-    itemsCountForPage: number
-    totalCount: number
-    onChange: (page: number, count: number) => void
-}
+    id?: string;
+    page: number;
+    itemsCountForPage: number;
+    totalCount: number;
+    onChange: (page: number, count: number) => void;
+};
 
-const SuperPagination: React.FC<SuperPaginationPropsType> = (
-    {
-        page, itemsCountForPage, totalCount, onChange, id = 'hw15',
-    }
-) => {
-    const lastPage = 10 // пишет студент // вычислить количество страниц
+const SuperPagination: React.FC<SuperPaginationPropsType> = ({
+    page,
+    itemsCountForPage,
+    totalCount,
+    onChange,
+    id = 'hw15',
+}) => {
+    const lastPage = Math.ceil(totalCount / itemsCountForPage);
 
     const onChangeCallback = (event: any, page: number) => {
-        // пишет студент
-    }
+        onChange(page, itemsCountForPage);
+    };
 
     const onChangeSelect = (event: any) => {
-        // пишет студент
-    }
+        onChange(1, event.target.value);
+    };
 
     return (
         <div className={s.pagination}>
             <Pagination
                 id={id + '-pagination'}
                 sx={{
-                    // стили для Pagination // пишет студент
+                    '& .MuiPaginationItem-root': {
+                        color: '#0d0b0c',
+                    },
+                    '& .Mui-selected': {
+                        backgroundColor: '#0066CC',
+                        color: '#ffffff',
+                        margin: 0,
+                    },
                 }}
                 page={page}
                 count={lastPage}
                 onChange={onChangeCallback}
                 hideNextButton
                 hidePrevButton
+                shape='rounded'
             />
 
-            <span className={s.text1}>
-                показать
-            </span>
+            <span className={s.text1}>показать</span>
 
             <SuperSelect
                 id={id + '-pagination-select'}
                 value={itemsCountForPage}
                 options={[
-                    {id: 4, value: 4},
-                    {id: 7, value: 7},
-                    {id: 10, value: 10},
+                    { id: 4, value: 4 },
+                    { id: 7, value: 7 },
+                    { id: 10, value: 10 },
                 ]}
                 onChange={onChangeSelect}
             />
 
-            <span className={s.text2}>
-                строк в таблице
-            </span>
+            <span className={s.text2}>строк в таблице</span>
         </div>
-    )
-}
+    );
+};
 
-export default SuperPagination
+export default SuperPagination;
